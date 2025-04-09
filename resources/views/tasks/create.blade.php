cff1951735f84ae58145b4f7da7340708a8b740e
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2025-04-08 22:36:25 -0300

    atual 80-04

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,6 +4,37 @@
             Voltar
         </a>
     </x-slot:btn>
-    Tela de Criar uma tarefa
+    <section id="create_task_section">
+        <h1>Criar tarefa</h1>
+        <form>
+            <div class="inputArea">
+                <label for="title">
+                    Título da Task
+                </label>
+                <input id="title" name="title" required placeholder="Digite o título da tarefa!" />
+            </div>
+            <div class="inputArea">
+                <label for="due_date">
+                    Data de Realização
+                </label>
+                <input type="date" id="due_date" name="due_date" required placeholder="Digite o título da tarefa!" />
+            </div>
+            <div class="inputArea">
+                <label for="category">
+                    Categoria
+                </label>
+                <select id="category" name="category" required>
+                    <option selected disabled value=""/>Selecione a categoria </option>
+                    <option> Valor qualquer </option>
+                </select>
+            </div>
+            <div class="inputArea">
+                <label for="title">
+                    Descrição da tarefa
+                </label>
+               <textarea name="description" placeholder="Digite uma descrição para sua task"></textarea>
+            </div>
+        </form>
+    </section>
 
 </x-layout>
