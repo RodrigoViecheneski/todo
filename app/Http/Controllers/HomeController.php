7c40b057136826ea532d311610c546b2fefc4445
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2025-04-24 18:12:31 -0300

    24-04 final projeto/somente falta atualizar imagem das tasks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,22 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Task;
-
+use Carbon\Carbon;
 class HomeController extends Controller
 {
     public function index(Request $request){
         //$tasks = Task::all()->take(3);
-        $data['tasks'] = Task::whereDate('due_date', date('Y-m-d'))->get();
+        if($request->date){
+            $filteredDate = $request->date;
+        }else{
+            $filteredDate = date('Y-m-d');
+        }
+        $carbonDate = Carbon::createFromDate($filteredDate);
+
+        $data['date_as_string'] = $carbonDate->translatedFormat('d').' de '.ucfirst($carbonDate->translatedFormat('M'));
+        $data['date_prev_button'] = $carbonDate->addDay(-1)->format('Y-m-d');
+        $data['date_next_button'] = $carbonDate->addDay(2)->format('Y-m-d');
+        $data['tasks'] = Task::whereDate('due_date', $filteredDate)->get();
         //$data['tasks'] = Task::whereDate('due_date', date('Y-m-d'));
         //dd($data['tasks']);
         $data['AuthUser'] = Auth::user();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,25 +12,31 @@
             <h2>Progresso do Dia -</h2>
             <div class="graph_header-line"></div>
             <div class="graph_header-date">
-                <img src="/assets/images/icon-prev.png"/>
-                    01 de abr
-                <img src="/assets/images/icon-next.png"/>
+                <a href="{{route('home', ['date'=> $date_prev_button])}}">
+                    <img src="/assets/images/icon-prev.png"/>
+                </a>
+                    {{$date_as_string}}
+                <a href="{{route('home', ['date'=> $date_next_button])}}">
+                    <img src="/assets/images/icon-next.png"/>
+                </a>
             </div>
         </div>
         <div class="graph_header-subtitle">
-            Tarefas: <b>3/6</b>
+            Tarefas: <b>{{$tasks_count-$undone_tasks_count}}/{{$tasks_count}}</b>
         </div>
         <div class="graph-placeholder">
         </div>
         <div class="tasks_left_footer">
             <img src="/assets/images/icon-info.png">
-            Restam 3 tarefas para serem realizadas
+            Restam {{$tasks_count-$undone_tasks_count}} tarefas para serem realizadas
         </div>
     </section>
     <section class="list">
         <div class="list_header">
-            <select class="list_header-select">
-                <option value="1">Todas as tarefas</option>
+            <select class="list_header-select" onChange="changeTaskStatusFilter(this)">
+                <option value="all_task">Todas as tarefas</option>
+                <option value="task_pending">Tarefas pendentes</option>
+                <option value="task_done">Tarefas realizadas</option>
             </select>
         </div>
         <div class="task_list">
@@ -41,6 +47,28 @@
 
         </div>
     </section>
+    <script>
+        function changeTaskStatusFilter(e) {
+            if(e.value == 'task_pending'){
+                showAllTasks();
+                document.querySelectorAll('.task_done').forEach(function(element){
+                    element.style.display = 'none';
+                })
+            }else if(e.value == 'task_done'){
+                showAllTasks();
+                document.querySelectorAll('.task_pending').forEach(function(element){
+                    element.style.display = 'none';
+                })
+            }else{
+                showAllTasks();
+            }
+        }
+        function showAllTasks() {
+            document.querySelectorAll('.task').forEach(function(element){
+                element.style.display = 'block';
+            })
+        }
+    </script>
 
     <script>
         async function taskUpdate(element){
@@ -64,6 +92,7 @@
            } else {
             element.checked = !status;
            }
+           location.reload();
          }
     </script>
 </x-layout>
